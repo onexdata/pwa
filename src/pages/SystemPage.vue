90e87bebd6829f0b87a5eefe8e29e16d59ede53c
Nick Steele <njsteele@gmail.com>
2024-12-26 20:53:44 -0500

    feat: Add SystemPage component to project structure

---
+++ src/pages/SystemPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>System settings</h1>
+
+    <div>Quasar v{{ $q.version }}</div>
+
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
